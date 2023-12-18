3d01e82c869ee89201bd47bda896725f422469b1
theand80 <the.and80@yahoo.com>
2023-12-18 22:32:25 +0000

    Kedai-kopi-23

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// toggle hamburger menu diklik
+const navbarNav = document.querySelector('.navbar-nav');
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+}
+
+// klik diluar hamburger menu
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
